29018209119eaca6570d3b83ca85efe83824d9b8
Sergi0iv <rickitir2.0@gmail.com>
2024-10-09 22:26:31 +0300

    Update Module_6_hard.py

--- Module_6_hard.py
+++ Module_6_hard.py
@@ -1,7 +1,10 @@
+import math
+
+
 class Figure:
     sides_count = 0
 
-    def __init__(self, *sides, color, filled):
+    def __init__(self, color, filled, *sides):
         self.__sides = sides
         self.__color = color
         self.filled = filled
@@ -19,13 +22,15 @@ class Figure:
             self.__color = r, g, b
 
     def __is_valid_sides(self, *sides):
+        if len(sides) != self.sides_count:
+            return False
         for side in sides:
             if not isinstance(side, int) or side <= 0:
                 return False
         return True
 
     def set_sides(self, *sides):
-        if self.__is_valid_sides(*sides):
+        if self.__is_valid_sides(sides):
             self.__sides = sides
 
     def get_sides(self):
@@ -40,12 +45,12 @@ class Circle(Figure):
     __radius = 0
 
     def __init__(self, color, sides):
-        Figure.__init__(self, color, sides)
-
+        self.sides = sides
+        super().__init__(self, color, sides)
+        self.__radius = sides / (2 * math.pi)
 
 
     def get_square(self):
-        self.__radius = self.sides / (2 * math.pi)
         return self.__radius
 
 
@@ -54,22 +59,26 @@ class Triangle(Figure):
     sides_count = 3
 
     def __init__(self, color, *sides):
-        Figure.__init__(self, color, *sides)
+        super().__init__(self, color, *sides)
 
     def get_square(self):
-        h = self.sides_count / (2 * self.sides)
-        S = (self.sides * h) / 2
+        h = self.sides_count / int(2 * self.sides)
+        S = self.sides * h / 2
         return S
 
 
-class  Cube(Figure):
+class Cube(Figure):
     sides_count = 12
 
-    def __init__(self):
-        Figure.__init__(self)
+    def __init__(self, color, *sides):
+        self.sides = sides
+        Figure.__init__(self, color, *sides)
 
     def get_volume(self):
-        pass
+        S = self.sides_count * self.sides
+        V = S[0]**3
+
+        return V
 
 
 circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
@@ -86,6 +95,12 @@ cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
 print(cube1.get_sides())
 circle1.set_sides(15) # Изменится
 print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
 
 # Проверка периметра (круга), это и есть длина:
 print(len(circle1))
